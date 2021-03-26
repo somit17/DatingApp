5f35ebbadee8c2585ce74e5b7fcd887332996af6
Somit <somitmeshram3256@gmail.com>
2021-03-26 23:57:37 +0530

    Added alertify Plugin

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,34 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertifyService } from '../services/alertify/alertify.service';
 import { AuthService } from '../services/auth-service/auth.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor(private authService:AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   login() {
-
-    this.authService.login(this.model).subscribe(data=>{
-      console.log('logged in success');
-    },error=>{
-      console.log('Failed');
-    })
+    this.authService.login(this.model).subscribe(
+      (data) => {
+        this.alertify.success('Login Successful');
+      },
+      (error) => {
+        this.alertify.error(error);
+      }
+    );
   }
 
-  loggedIn(){
-    const token = localStorage.getItem('token')
-    return token!=undefined?true:false;
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return token != undefined ? true : false;
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('token');
-    console.log('User Logged Out!')
+    console.log('User Logged Out!');
   }
 }
